584442d8c0e4d501e0fc00ad886b35926c22afeb
Dave <daffataira@gmail.com>
2022-12-19 21:20:23 +0700

    insert data to listenme

--- HumanResource.sql
+++ HumanResource.sql
@@ -16,6 +16,16 @@ CREATE TABLE IF NOT EXISTS employees(
     PRIMARY KEY(employee_id)
 );
 
+INSERT INTO Employee (employee_id) VALUES('0001','0002','0003','0004','0005','0006','0007','0008','0009','0010','0011');
+INSERT INTO Employee (employee_nik) VALUES('2102972','2102973','2102974','2102974','2102975','2102976','2102977','2102978','2102979','2102980','2102981','2102982');
+INSERT INTO Employee (employee_name) VALUES('Nuriansyah','Zaidan','Daffa','Dylee','Genta','Mackenzie','Michelle','Irene','Agatha','Azka','Rafi');
+INSERT INTO Employee (employee_gender) VALUES('L','L','L','L','L','W','W','W','W','L','L');
+INSERT INTO Employee (employee_birthplace) VALUES('Jakarta','Bandung','Palembang','Sydney','Osaka','Tokyo','Jakarta','Medan','Guangzhou','Bandung','Jogja');
+INSERT INTO Employee (employee_birthdate) VALUES('21-03-2002','03-04-1999','12-05-2000','16-06-2001','13-07-1998','24-08-2002','09-08-2000','01-01-2003','13-09-2001','06-10-1999','27-11-2002');
+INSERT INTO Employee (employee_address) VALUES('Jl.Semanggi no 05','Jl.Rambutan no 25C','Jl.Bali no 12A','Jl.Belitung no 08','Jl.Aceh no 41','Jl.Tamansari no 12','Jl.Prindapan no 24','Jl.Medan no 11','Jl.Soekarno','Jl.Pasturi no 33','Jl.Merdeka no 21');
+INSERT INTO Employee (employee_phonenumber) VALUES('08184451','08723456','08213675','08125643','08123864','08123556','08927575','08216535','08676356','08978565','08123434');
+INSERT INTO Employee (employee_joindate) VALUES('20-03-2021','13-04-2021','09-04-2021','27-06-2021','15-03-2021','19-01-2021','11-08-2021','23-09-2021','26-10-2021','08-03-2021','20-10-2021');
+
 -- Tabel Position
 CREATE TABLE IF NOT EXISTS positions(
     position_id int(10) NOT NULL AUTO_INCREMENT,
@@ -26,12 +36,20 @@ CREATE TABLE IF NOT EXISTS positions(
     FOREIGN KEY(position_parent) REFERENCES positions(position_id)
 );
 
+INSERT INTO Position (position_id) VALUES('1','2','3','4','5');
+INSERT INTO Position (position_name) VALUES('Komisaris','CEO','Direktur Utama','Manager','Karyawan');
+INSERT INTO Position (position_level) VALUES('1','2','2','3','4');
+INSERT INTO Position (position_parent) VALUES('-','1','1','3','4');
+
 -- Tabel Department
 CREATE TABLE IF NOT EXISTS departments(
     department_id int(10) NOT NULL AUTO_INCREMENT,
     department_name varchar(255) NOT NULL,
     PRIMARY KEY(department_id)
 );
+
+INSERT INTO Department (department_id) VALUES('1','2','3','4','5','6');
+INSERT INTO Department (department_name) VALUES('Keuangan','Marketing','Advertising',"Produksi",'Kepegawaian','Bagian Umum');
 
 -- Tabel Employee Position
 CREATE TABLE IF NOT EXISTS employee_position(
@@ -47,6 +65,11 @@ CREATE TABLE IF NOT EXISTS employee_position(
     FOREIGN KEY(supervisor_id) REFERENCES employees(employee_id)
 );
 
+INSERT INTO employee_position (employee_position_id) VALUES('1','2','3','4','5','06','7','8','9','10','11');
+INSERT INTO employee_position (employee_id) VALUES('0001','0010','0007','0011','0002','0003','0004','0005','0006','0008','0008','0009');
+INSERT INTO employee_position (department_id) VALUES('-','-','-','4','5','6','3','1','2','5','3');
+INSERT INTO employee_position (position_id) VALUES('1','2','3','4','4','4','4','4','4','5','5');
+
 -- Tabel Salary Component
 CREATE TABLE IF NOT EXISTS salary_components(
     salary_component_id int(10) NOT NULL AUTO_INCREMENT,
@@ -54,6 +77,10 @@ CREATE TABLE IF NOT EXISTS salary_components(
     salary_component_type varchar(255) NOT NULL,
     PRIMARY KEY(salary_component_id)
 );
+
+INSERT INTO salary_components (salary_component_id) VALUES('1','2','3','4','5','6','7','8');
+INSERT INTO salary_components (salary_component_name) VALUES('Gaji','Tunjangan Makan','Tunjangan Kesehatan','Tunjangan Transpotasi','Paket berlangganan ListenMe','Pajak Penghasilan','THR','Bonus');
+INSERT INTO salary_components (salary_component_type) VALUES('allowance','allowance','allowance','allowance','allowance','Deducation','allowance','allowance');
 
 -- Tabel Additional Salary
 CREATE TABLE IF NOT EXISTS additional_salaries(
@@ -67,6 +94,12 @@ CREATE TABLE IF NOT EXISTS additional_salaries(
     FOREIGN KEY(salary_component_id) REFERENCES salary_components(salary_component_id)
 );
 
+INSERT INTO additional_salaries (additional_salary_id) VALUES('1','2','3','4','5','6','7','8');
+INSERT INTO additional_salaries (employee_id) VALUES('0003','0003','0004','0004','0005','0005','0006','0006');
+INSERT INTO additional_salaries (salary_component_id) VALUES('8','7','8','7','8','7','8','7');
+INSERT INTO additional_salaries (additional_salary_period) VALUES('June 2022','May 2022','June 2022','May 2022','June 2022','May 2022','June 2022','May 2022');
+INSERT INTO additional_salaries (additional_salary_amount) VALUES('Rp.5.000.000','Rp.7.500.000','Rp.5.000.000','Rp.7.500.000','Rp.5.000.000','Rp.7.500.000','Rp.5.000.000','Rp.7.500.000');
+
 -- Tabel Salary Employee
 CREATE TABLE IF NOT EXISTS salary_employees(
     salary_employee_id int(10) NOT NULL AUTO_INCREMENT,
@@ -78,12 +111,20 @@ CREATE TABLE IF NOT EXISTS salary_employees(
     FOREIGN KEY(salary_component_id) REFERENCES salary_components(salary_component_id)
 ); 
 
+INSERT INTO salary_employees (salary_employee_id) VALUES('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','37','38','39','40','41','42','43','44','45','46','47','48');
+INSERT INTO salary_employees (employee_id) VALUES('0002','0002','0002','0002','0002','0002','0003','0003','0003','0003','0003','0003','0004','0004','0004','0004','0004','0004','0005','0005','0005','0005','0005','0005','0006','0006','0006','0006','0006','0006','0008','0008','0008','0008','0008','0008','0009','0009','0009','0009','0009','0009','0011','0011','0011','0011','0011','0011');
+INSERT INTO salary_employees (salary_component_id) VALUES('1','2','3','4','5','6','1','2','3','4','5','6''1','2','3','4','5','6''1','2','3','4','5','6''1','2','3','4','5','6''1','2','3','4','5','6''1','2','3','4','5','6''1','2','3','4','5','6');
+INSERT INTO salary_employees (additional_salary_amount) VALUES('Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000');
+
 -- Tabel Payroll
 CREATE TABLE IF NOT EXISTS payrolls(
     payroll_id int(10) NOT NULL AUTO_INCREMENT,
     payroll_period int(2) NOT NULL,
     PRIMARY KEY(payroll_id)
 );
+
+INSERT INTO Payroll (payroll_id) VALUES('1');
+INSERT INTO Payroll (payroll_period) VALUES('June 2022');
 
 -- Tabel Payroll Detail
 CREATE TABLE IF NOT EXISTS payroll_details(
@@ -97,3 +138,9 @@ CREATE TABLE IF NOT EXISTS payroll_details(
     FOREIGN KEY(employee_id) REFERENCES employees(employee_id),
     FOREIGN KEY(salary_component_id) REFERENCES salary_components(salary_component_id)
 );
+
+INSERT INTO payroll_details (payroll_detail_id) VALUES('1','2','3','4','5','6','7');
+INSERT INTO payroll_details (payroll_id) VALUES('1','1','1','1','1','1','1');
+INSERT INTO payroll_details (employee_id) VALUES('0003','0003','0003','0003','0003','0003','0003');
+INSERT INTO payroll_details (salary_component_id) VALUES('1','2','3','4','5','6','8');
+INSERT INTO payroll_details (amount) VALUES('Rp.15.000.000','Rp.2.000.000','Rp.5.000.000','Rp.1.000.000','Rp.500.000','Rp.250.000','Rp.5.000.000');
--- ListenMe.sql
+++ ListenMe.sql
@@ -11,6 +11,11 @@ CREATE TABLE IF NOT EXISTS Bundlings(
     PRIMARY KEY (bundling_id)
 );
 
+INSERT INTO Bundling (bundling_id) VALUES ('1','2','3','4')
+INSERT INTO Bundling (bundling_name) VALUES ('Paket Harian','Paket Mingguan','Paket Bulanan','Paket Tahunan')
+INSERT INTO Bundling (bundling_price) VALUES ('Rp.2.000','Rp.7.500','Rp.45.000','Rp.250.000')
+INSERT INTO Bundling (access_level) VALUES ('1','2','3','4')
+
 -- Tabel User
 CREATE TABLE IF NOT EXISTS Users(
     user_id int(10) NOT NULL AUTO_INCREMENT,
@@ -24,6 +29,14 @@ CREATE TABLE IF NOT EXISTS Users(
     FOREIGN KEY (bundling_id) REFERENCES Bundlings(bundling_id)
 );
 
+INSERT INTO User (user_id) VALUES ('1');
+INSERT INTO User (username) VALUES ('zeddonly');
+INSERT INTO User (password) VALUES ('aezakmi123');
+INSERT INTO User (name) VALUES ('zedd');
+INSERT INTO User (gender) VALUES ('L');
+INSERT INTO User (email) VALUES ('dunno@gmail.com');
+INSERT INTO User (bundling_id) VALUES ('1');
+
 -- Tabel Payment Methods
 CREATE TABLE IF NOT EXISTS PaymentMethods(
     payment_methods_id int(10) NOT NULL AUTO_INCREMENT,
@@ -31,6 +44,10 @@ CREATE TABLE IF NOT EXISTS PaymentMethods(
     payment_methods_details varchar(255) NOT NULL,
     PRIMARY KEY(payment_methods_id)
 );
+
+INSERT INTO PaymentMethods (payment_methods_id) VALUES ('001','002','003','004')
+INSERT INTO PaymentMethods (payment_methods_name) VALUES ('Virtual Account','Bank Transfer','E-Wallet','Debit Card')
+INSERT INTO PaymentMethods (payment_methods_details) VALUES ('')
 
 -- Tabel Transaction
 CREATE TABLE IF NOT EXISTS transactions(
@@ -44,12 +61,21 @@ CREATE TABLE IF NOT EXISTS transactions(
     FOREIGN KEY (payment_methods_id) REFERENCES PaymentMethods(payment_methods_id)
 );
 
+INSERT INTO transactions (transactions_id) VALUES ('1')
+INSERT INTO transactions (user_id) VALUES ('1')
+INSERT INTO transactions (payment_methods_id) VALUES ('001')
+INSERT INTO transactions (amount) VALUES ('Rp.250.000')
+INSERT INTO transactions (is_paid) VALUES ('1')
+
 -- Tabel Genre
 CREATE TABLE IF NOT EXISTS genres(
     genre_id int(10) NOT NULL AUTO_INCREMENT,
     genre_name varchar(255) NOT NULL,
     PRIMARY KEY(genre_id)
 );
+
+INSERT INTO Genre (genre_id) VALUES ('1','2')
+INSERT INTO Genre (genre_name) VALUES ('R&B','Hip Hop')
 
 -- Tabel Artist 
 CREATE TABLE IF NOT EXISTS artists(
@@ -58,6 +84,10 @@ CREATE TABLE IF NOT EXISTS artists(
     artist_location varchar(255) NOT NULL,
     PRIMARY KEY(artist_id)
 );
+
+INSERT INTO Artist (artist_id) VALUES ('1','2','3')
+INSERT INTO Artist (artist_name) VALUES ('Harry Styles','One Direction','DJ Snake')
+insert INTO Artist (artist_location) VALUES ('U.K','U.K','France')
 
 -- Tabel Album
 CREATE TABLE IF NOT EXISTS albums(
@@ -68,6 +98,11 @@ CREATE TABLE IF NOT EXISTS albums(
     PRIMARY KEY(album_id),
     FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
 );
+
+INSERT INTO Album (album_id) VALUES ('001','002','003')
+INSERT INTO Album (album_name) VALUES ('Harrys House','Made in the A.M','Encore')
+INSERT INTO Album (artist_id) VALUES ('1','2','3')
+INSERT INTO Album (year) VALUES ('2022','2015','2016')
 
 -- Tabel Song 
 CREATE TABLE IF NOT EXISTS songs(
@@ -82,6 +117,12 @@ CREATE TABLE IF NOT EXISTS songs(
     FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
 );
 
+INSERT INTO Song (song_id) VALUES ('1','2','3')
+INSERT INTO Song (song_title) VALUES ('As it was','History','Middle')
+INSERT INTO Song (album_id) VALUES ('001','002','003')
+INSERT INTO Song (genre_id) VALUES ('1','2','3')
+INSERT INTO Song (song_duration) VALUES ('2 menit','3 menit','2 menit')
+
 -- Tabel Playlist
 CREATE TABLE IF NOT EXISTS playlists(
     playlist_id int(10) NOT NULL AUTO_INCREMENT,
@@ -92,6 +133,11 @@ CREATE TABLE IF NOT EXISTS playlists(
     FOREIGN KEY(song_id) REFERENCES songs(song_id),
     FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
+
+INSERT INTO Playlist (playlist_id) VALUES ('01','02')
+INSERT INTO Playlist (playlist_name) VALUES ('Mood','Chill day')
+INSERT INTO Playlist (user_id) VALUES ('1','1')
+
 
 -- Tabel Song Play
 CREATE TABLE IF NOT EXISTS songplays(
@@ -104,3 +150,8 @@ CREATE TABLE IF NOT EXISTS songplays(
     FOREIGN KEY(playlist_id) REFERENCES playlists(playlist_id),
     FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
+
+INSERT INTO songplays (song_play_id) VALUES ('1','2','3')
+INSERT INTO songplays (song_id) VALUES ('1','2','3')
+INSERT INTO songplays (playlist_id) VALUES ('01','02','03')
+INSERT INTO songplays (user_id) VALUES ('1','1','1')
